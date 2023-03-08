d7ac4d54b7e47f8da0188b282be73459f8e97d64
Cesar Mejia <cmejia@gmail.com>
2023-03-09 03:47:58 +0700

    upload

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,22 @@
+import { WAKA_APP_SECRET } from '$env/static/private'
 import type { PageServerLoad } from './$types'
 
-export const load = (async (event) => {
-  console.log('event:', event)
-  return {}
+const baseUrl = 'https://wakatime.com/api/v1/'
+
+export const load = (async ({ fetch }) => {
+  try {
+    const res = await fetch(`${baseUrl}users/cesarnml/all_time_since_today`, {
+      headers: {
+        Authorization: `Basic ${WAKA_APP_SECRET}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    console.log('res:', res)
+    if (res.ok) {
+      return res.json()
+    }
+    throw new Error('Network response was not ok')
+  } catch (err) {
+    console.error({ err })
+  }
 }) satisfies PageServerLoad
